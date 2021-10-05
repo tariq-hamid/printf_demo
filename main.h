fd203394fec548aff90bc69b042f60a5d7ae05df
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-05 17:08:12 +0300

    implemented a map struct, declared handlers and helpers functions

--- main.h
+++ main.h
@@ -1,7 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h>
+
+/**
+ * struct map - a struct to map format specifier
+ * with their corresponding function
+ *
+ * @specifier: char representing format specifiers
+ * @func: address of corresponding function
+ */
+typedef struct map_struct
+{
+	char specifier;
+	void (*func)(va_list args, int *count);
+} map;
+
 /* main function */
 int _printf(const char *format, ...);
+
+/* helpers1.c */
+void _putchar(char, int *);
+void (*mapper(char format))(va_list, int *);
+void print(char *str, int *count);
+
+/* handlers1.c */
+void print_c(va_list args, int *count);
+void print_s(va_list args, int *count);
 
 #endif
