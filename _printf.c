4077588a8440086d1f9c57a387bbab4e11ab953c
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-05 16:21:26 +0300

    added basic backbone

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - equivalent of the printf function
+ * in the standard library
+ * @format: characters and format specifiers
+ *
+ * Return: count of printed characters, int
+ */
+int _printf(const char *format, ...)
+{
+
+}
