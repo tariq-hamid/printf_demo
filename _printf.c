e501d98aea5cdafb866688ee89d68d85ec0b9874
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-05 17:05:50 +0300

    implemented the logic

--- _printf.c
+++ _printf.c
@@ -9,5 +9,48 @@
  */
 int _printf(const char *format, ...)
 {
+    int count = 0;
+    va_list args;
+    void (*func)(va_list args, int *count);
 
+    va_start(args, format);
+    if ( format == NULL || (*format == '%' && *(format + 1) == '\0') )
+        return (-1);
+    
+    while ( (*format) != '\0' )
+	{
+		if ( ((*format) == '%') && (*(format + 1) != '\0') )
+		{
+			format++;
+			func = mapper(*format);
+			if ( func != NULL )
+			{
+				func(args, &count);
+				format++;
+			}
+			else if ( *format == '%' )
+			{
+				_putchar(*format, &count);
+				format++;
+			}
+			else
+			{
+				_putchar('%', &count);
+				_putchar(*format, &count);
+				format++;
+			}
+		}
+		else if ( *format != '%' )
+		{
+			_putchar( *format, &count );
+			format++;
+		}
+		else
+        {
+			format++;
+        }
+	}
+
+    va_end(args);
+	return (count);
 }
