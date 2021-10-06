43ea8abb13be1297f10307648409220a73a8fdc4
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-06 07:00:05 +0300

    added declaration of new implemented helpers and handlers

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct map - a struct to map format specifier
@@ -25,9 +26,12 @@ int _printf(const char *format, ...);
 void _putchar(char, int *);
 void (*mapper(char format))(va_list, int *);
 void print(char *str, int *count);
+int numlen(int n, int base);
+void itoa_(int num, char* str, int radix);
 
 /* handlers1.c */
 void print_c(va_list args, int *count);
 void print_s(va_list args, int *count);
+void print_int(va_list args, int *count);
 
 #endif
